95c0dd8cf3f7705362a633fcf7bdc5bab6b7ba9b
AlexVipond <alex.r.vipond@gmail.com>
2022-03-23 11:30:05 -0500

    clone template

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="h-screen w-screen flex items-center justify-center bg-gray-900">
+    <h1 class="text-2xl tracking-widest font-bold uppercase text-gray-400">TITLE</h1>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
